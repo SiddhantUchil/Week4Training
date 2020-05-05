bbd139b87a1bc7ad6b2693e9e6907377d785623b
Siddhant Uchil <siddhant.uchil@somaiya.edu>
2020-05-05 21:16:42 +0530

    2nd commit

--- Week4Training.R
+++ Week4Training.R
@@ -15,3 +15,22 @@ summary(plants)
 table(plants$Active_Growth_Period)
 str(plants)
 
+?sample
+sample(1:6, 4, replace = TRUE)
+sample(1:20, 10 )
+LETTERS
+sample(LETTERS)
+flips <- sample(c(0,1), 100, replace = TRUE, prob = c(0.3,0.7))
+sum(flips)
+?rbinom
+rbinom(1, size = 100, prob = 0.7)
+flips2 <- rbinom(100, 1, prob = 0.7)
+sum(flips2)
+?rnorm
+rnorm(10)
+rnorm(10, mean = 100, sd = 25)
+rpois(5 , 10)
+my_pois <- replicate(100, rpois(5,10))
+my_pois
+cm <- colMeans(my_pois)
+hist(cm)
